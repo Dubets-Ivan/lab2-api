8a752da56f6039c4ec2e54c0cd5fea85c7564007
dubetsivan <vaniadubets16@gmail.com>
2026-04-28 10:54:54 +0300

    add db link

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_FcWfB0eOT1Xh@ep-gentle-recipe-ajotvudy.c-3.us-east-2.aws.neon.tech/neondb?sslmode=require")
 
 engine = create_engine(DATABASE_URL)
 
